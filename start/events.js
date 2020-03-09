f6da51dc9da665408e44e599173b2272f9b88175
ezequiellivingston <pabloezelivingston@gmail.com>
2020-03-09 12:57:48 -0300

    add handle for initial user

--- start/events.js
+++ start/events.js
@@ -3,16 +3,18 @@ const User = use("App/Models/User");
 
 initialApp();
 async function initialApp() {
-  await User.findOrCreate(
-    { email: "admin@uctopus.com" },
-    {
-      email: "admin@uctopus.com",
-      username: "admin",
-      name: "admin",
-      lastname: "admin",
-      password: "admin",
-      admin: true
-    }
-  );
-  console.log("User admin success");
+  try {
+    await User.findOrCreate(
+      { email: "admin@uctopus.com" },
+      {
+        email: "admin@uctopus.com",
+        username: "admin",
+        name: "admin",
+        lastname: "admin",
+        password: "admin",
+        admin: true
+      }
+    );
+    console.log("User admin success");
+  } catch (error) {}
 }
